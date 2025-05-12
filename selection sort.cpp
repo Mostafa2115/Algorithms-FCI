4ec3a02dcf45da10d97f5495a614aa6eac0e7a04
Mostafa_Mahmoud <157222523+Mostafa2115@users.noreply.github.com>
2025-05-12 19:18:07 +0300

    Update selection sort.cpp

--- selection sort.cpp
+++ selection sort.cpp
@@ -1,4 +1,7 @@
- int n;
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+int n;
     cin >> n;
     int arr[n];
     for (int i = 0; i < n; i++)
@@ -16,3 +19,4 @@
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
     cout << endl;
+}
